9a6a25efc44fe54d5cf5c7e049775b0f1d7117ae
ClaraNocilla <claranocilla12@gmail.com>
2022-03-16 12:17:12 +0100

    fixed snack 2

--- Snack 2/main.js
+++ Snack 2/main.js
@@ -5,9 +5,9 @@ let parola2 = prompt("inserisci seconda parola");
 
 
 if (parola1.length > parola2.length) {
-    document.getElementById("container").innerHTML = `${parola1}`;
+    document.getElementById("container").innerHTML = `${parola2} e ${parola1}`;
 } else if (parola1.length < parola2.length) {
-    document.getElementById("container").innerHTML = `${parola2}`;
+    document.getElementById("container").innerHTML = `${parola1} ${parola2}`;
 } else {
     document.getElementById("container").innerHTML = `Le parole hanno la stessa lunghezza`;
 }
