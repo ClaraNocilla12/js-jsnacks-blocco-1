3f258fd79023ab03e5f161e4d9d8de960c833bf6
ClaraNocilla <claranocilla12@gmail.com>
2022-03-16 11:56:43 +0100

    conversion

--- Snack 1/main.js
+++ Snack 1/main.js
@@ -1,8 +1,14 @@
 let n1 = prompt("inserisci primo numero");
 let n2 = prompt("inserisci secondo numero");
 
+n1=parseInt(n1);
+n2 = parseInt(n2);
+
+
 if (n1 > n2) {
-    document.getElementById("container").innerHTML = `${n1}`
-} else {
-    document.getElementById("container").innerHTML = `${n2}`
+    document.getElementById("container").innerHTML = `${n1}`;
+} else if(n2 > n1){
+    document.getElementById("container").innerHTML = `${n2}`;
+} else{
+    document.getElementById("container").innerHTML = `I numeri sono uguali`;
 }
